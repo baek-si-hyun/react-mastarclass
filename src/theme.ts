3f12308a528ea503f6f60e7a7072c113cc79fc55
baek-si-hyun <qortlgus100@gmail.com>
2023-01-05 01:55:09 +0900

    react-masterclass

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  textColor: "white",
+  bgColor: "black",
+  btnColor:"tomato"
+};
+export const darkTheme: DefaultTheme = {
+  textColor: "black",
+  bgColor: "white",
+  btnColor:"teal"
+};
